aecadd44a8ab740a6ae4446e376a750d56c1877e
katteez <ekaterina.zoschik@gmail.com>
2022-04-26 22:19:16 +0300

    1.3. Разделила код на модули

---
+++ src/view/list-empty-view.js
@@ -0,0 +1,21 @@
+import { createElement } from '../render.js';
+
+const createListEmptyTemplate = () => '<p class="trip-events__msg">Click New Event to create your first point</p>';
+
+export default class ListEmptyView {
+  getTemplate() {
+    return createListEmptyTemplate();
+  }
+
+  getElement() {
+    if(!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
