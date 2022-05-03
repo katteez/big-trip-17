38bc16a21119226dacacd702c4779a83da9c6aa9
katteez <ekaterina.zoschik@gmail.com>
2022-05-03 19:53:51 +0300

    2.1. Проставила опциям в PointEditView правильные значения атрибутов
    id, for, name

--- src/view/point-edit-view.js
+++ src/view/point-edit-view.js
@@ -31,10 +31,10 @@ const createPointEditViewOfferSelectorsTemplate = (offers, selectedOffers) => of
 
   return (
     `<div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage"
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.id}" type="checkbox" name="event-offer-${offer.id}"
         ${checked}
       >
-      <label class="event__offer-label" for="event-offer-luggage-1">
+      <label class="event__offer-label" for="event-offer-${offer.id}">
         <span class="event__offer-title">${offer.title}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
