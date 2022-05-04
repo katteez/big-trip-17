98733992d94164882eb6af1a5639fff5ff2b5f33
katteez <ekaterina.zoschik@gmail.com>
2022-05-04 04:19:36 +0300

    3.1.4 Добавила доп. возможности закрыть форму
    - добавила обработчик отправки
    - добавила обработчик клавиши Esc, который будет закрывать форму
    - описала логику добавления и удаления этого обработчика

--- src/presenter/points-presenter.js
+++ src/presenter/points-presenter.js
@@ -37,20 +37,41 @@ export default class PointsPresenter {
     const pointComponent = new PointView(offersByPointType, point);
     const pointEditComponent = new PointEditView(offersByPointType, point);
 
+    // Открываем форму редактирования
     const replacePointToForm = () => {
       this.#pointListComponent.element.replaceChild(pointEditComponent.element, pointComponent.element);
     };
 
+    // Закрываем форму редактирования
     const replaceFormToPoint = () => {
       this.#pointListComponent.element.replaceChild(pointComponent.element, pointEditComponent.element);
     };
 
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    // Обработчик для открытия формы редактирования
     pointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
       replacePointToForm();
+      document.addEventListener('keydown', onEscKeyDown);
     });
 
+    // Обработчик для закрытия формы редактирования
     pointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
       replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    });
+
+    // Отправка отредактированных данных
+    pointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
     });
 
     render(pointComponent, this.#pointListComponent.element);
