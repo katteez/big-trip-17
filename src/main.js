acfd11e86ff387e5e725ee07da4a2f02e2750bf9
katteez <ekaterina.zoschik@gmail.com>
2022-05-09 23:58:27 +0300

    4.2.1 Отрисовала кнопку создания новой точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { render } from './framework/render.js';
 import FilterView from './view/filter-view.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 import PointsPresenter from './presenter/points-presenter.js';
 
 const siteHeaderElement = document.querySelector('.page-header .trip-main');
@@ -9,5 +10,6 @@ const siteMainElement = document.querySelector('.page-main .trip-events');
 const pointsPresenter = new PointsPresenter(siteHeaderElement, siteMainElement);
 
 render(new FilterView(), siteHeaderControlsElement);
+render(new NewPointButtonView(), siteHeaderElement);
 
 pointsPresenter.init();
--- src/mock/destination.js
+++ src/mock/destination.js
@@ -34,7 +34,6 @@ const generatePictures = () => {
   }
 
   return [];
-
 };
 
 export const generateDestination = () => ({
