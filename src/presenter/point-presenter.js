c44327d83c4ebaf564b8a37a1b0ac90668884220
katteez <ekaterina.zoschik@gmail.com>
2022-05-14 16:13:40 +0300

    5.1.3 Добавила возможность повторно инициализировать PointPresenter

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,4 +1,4 @@
-import { render, replace } from '../framework/render.js';
+import { render, replace, remove } from '../framework/render.js';
 import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 
@@ -17,6 +17,9 @@ export default class PointPresenter {
   init = (point, offersByPointType) => {
     this.#point = point;
 
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
+
     this.#pointComponent = new PointView(offersByPointType, point);
     this.#pointEditComponent = new PointEditView(offersByPointType, point);
 
@@ -24,7 +27,26 @@ export default class PointPresenter {
     this.#pointEditComponent.setClickHandler(this.#handleClick);
     this.#pointEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
 
-    render(this.#pointComponent, this.#pointListContainer);
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#pointListContainer);
+      return;
+    }
+
+    if (this.#pointListContainer.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#pointListContainer.contains(prevPointEditComponent.element)) {
+      replace(this.#pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+  };
+
+  destroy = () => {
+    remove(this.#pointComponent);
+    remove(this.#pointEditComponent);
   };
 
   // Открываем форму редактирования
